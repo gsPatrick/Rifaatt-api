d6e349341592f95592823fc465bdbd43f5b85abd
unknown <patrickgsiqueira@hotmail.com>
2026-03-12 06:59:20 -0300

    fix: secure instance and raffle routes with authMiddleware

--- src/Features/Instance/instance.routes.js
+++ src/Features/Instance/instance.routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const instanceController = require('./instance.controller');
 
-router.post('/init', instanceController.init);
-router.post('/:id/connect', instanceController.connect);
-router.get('/:id/status', instanceController.status);
-router.get('/', instanceController.list);
+const authMiddleware = require('../../Middlewares/auth');
+
+router.post('/init', authMiddleware, instanceController.init);
+router.post('/:id/connect', authMiddleware, instanceController.connect);
+router.get('/:id/status', authMiddleware, instanceController.status);
+router.get('/', authMiddleware, instanceController.list);
 
 module.exports = router;
--- src/Features/Raffle/raffle.routes.js
+++ src/Features/Raffle/raffle.routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const RaffleController = require('./raffle.controller');
 
-router.post('/', RaffleController.create);
-router.get('/groups', RaffleController.listGroups);
-router.get('/history', RaffleController.getHistory);
-router.get('/active/:groupJid', RaffleController.getActive);
-router.post('/finalize/:id', RaffleController.finalize);
-router.post('/create-group', RaffleController.createGroup);
-router.get('/groups-from-instance/:instanceId', RaffleController.getGroupsFromInstance);
+const authMiddleware = require('../../Middlewares/auth');
+
+router.post('/', authMiddleware, RaffleController.create);
+router.get('/groups', authMiddleware, RaffleController.listGroups);
+router.get('/history', authMiddleware, RaffleController.getHistory);
+router.get('/active/:groupJid', authMiddleware, RaffleController.getActive);
+router.post('/finalize/:id', authMiddleware, RaffleController.finalize);
+router.post('/create-group', authMiddleware, RaffleController.createGroup);
+router.get('/groups-from-instance/:instanceId', authMiddleware, RaffleController.getGroupsFromInstance);
 
 module.exports = router;
